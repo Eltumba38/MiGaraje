a9f3ad8d4ce77f1abddfa8936ed6dffa50a1be3f
Francisco Jose Sanchez Tapia <calamillorweb@hotmail.com>
2018-01-22 19:13:29 +0100

    Initial Commit

---
+++ Mi Garaje/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Mi Garaje
+//
+//  Created by Francisco Jose Sanchez Tapia on 22/1/18.
+//  Copyright © 2018 Francisco Jose Sanchez Tapia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
